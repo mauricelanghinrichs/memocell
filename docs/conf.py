318263e18acdd6dcfd78f1ca70db871bc5c3e515
mauricelanghinrichs <m.langhinrichs@icloud.com>
2021-05-21 14:50:49 +0200

    rdt | build fix update

--- docs/conf.py
+++ docs/conf.py
@@ -28,8 +28,6 @@ release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 
-autodoc_mock_imports = ['numpydoc']
-
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
